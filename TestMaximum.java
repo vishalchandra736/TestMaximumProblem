14771e01faca2bcf7cf9184af4e2f3dc5eec19d3
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-04 11:01:49 +0530

    Added : Extend parameter more than three using generic

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,24 +1,54 @@
 package com.blz.generices;
 
-public class TestMaximum {
-    public static void main(String[] args) {
-        String firstString = "Apple";
-        String secondString = "Peach";
-        String thirdString = "Banana";
-        System.out.println("Maximum among three strings");
-        TestMaximum testMaximum = new TestMaximum();
-        testMaximum.findMax(firstString, secondString, thirdString);
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestMaximum <T extends Comparable <T>>{
+    T varOne;
+    T varTwo;
+    T varThree;
+    T varFour;
+    T varFive;
+    public TestMaximum(T varOne, T varTwo, T varThree, T varFour, T varFive) {
+        super();
+        this.varOne = varOne;
+        this.varTwo = varTwo;
+        this.varThree = varThree;
+        this.varFour = varFour;
+        this.varFive = varFive;
     }
 
-    private void findMax(String firstString, String secondString, String thirdString) {
-        String maxString;
-        if (firstString.compareTo(secondString) > 0 && firstString.compareTo(thirdString) > 0) {
-            maxString = firstString;
-        } else if (secondString.compareTo(thirdString) > 0) {
-            maxString = secondString;
-        } else {
-            maxString = thirdString;
-        }
-        System.out.println("Maximum float is " + maxString + " among " + firstString + ", " + secondString + ", " + thirdString);
+    public void findMax() {
+        TestMaximum.max(this.varOne, this.varTwo, this.varThree, this.varFour, this.varFive);
+    }
+    public static void main(String[] args) {
+        Integer fNum = 11;
+        Integer sNum = 21;
+        Integer tNum = 31;
+        Integer num4 = 41;
+        Integer num5 = 51;
+        Float fNumFloat = 11.1F;
+        Float sNumFloat = 21.1F;
+        Float tNumFloat = 31.1F;
+        Float numF4 = 41.1F;
+        Float numF5 = 51.1F;
+        String fString = "Apple";
+        String sString = "Peach";
+        String tString = "Banana";
+        String string4 = "Mango";
+        String string5 = "PineApple";
+        System.out.println("***** Test Maximum among three ***** ");
+        max(fNum, sNum, tNum, num4, num5);
+        max(fNumFloat, sNumFloat, tNumFloat, numF4, numF5 );
+        max(fString, sString, tString, string4, string5);
+    }
+    public static <E extends Comparable<E>> void max(E firstVar, E secondVar, E thirdVar, E fourthVar, E fifthVar) {
+        ArrayList<E> varList = new ArrayList<>();
+        varList.add(firstVar);
+        varList.add(secondVar);
+        varList.add(thirdVar);
+        varList.add(fourthVar);
+        varList.add(fifthVar);
+        System.out.println(Collections.max(varList) + " is the maximum out of " + varList);
     }
 }
